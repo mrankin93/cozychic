7e9069702f6df194242c388177b8d2a04edf69e6
Michael Rankin <michaelrobertrankin@gmail.com>
2021-12-07 14:09:48 -0500

    Edit HTML, CSS, & JS - Edited HTML and added srcset for all images. Continued to style elements of newsletter. Added JS function for newsletter input for validation

--- js/app.js
+++ js/app.js
@@ -12,19 +12,19 @@ let app = {
   navMenu: document.getElementById("nav-menu"),
   // 2. hideSecondaryNav()
   clickOutsideNav: document.getElementsByClassName("close-secondary-nav"),
-  // 3. figcaption
+  // 3 & 4. figcaption
   coverCaption: document.getElementById("cover-caption"),
-  // 4. checkHover()
-  // 4.1. isHover() checks if element is hovered
+  // 5. checkHover()
+  // 5.1. isHover() checks if element is hovered
   isHover: e => e.parentElement.querySelector(':hover') === e,
   latestLooksList: document.getElementById('latest-looks-list'),
   scrollbarDiv: document.getElementById('scrollbar-div'),
-  // 4.2 addScroll() toggles classes to show scrollbar and hide empty div of same size to create overlay effect
+  // 5.2 addScroll() toggles classes to show scrollbar and hide empty div of same size to create overlay effect
   addScroll: e => {
     app.latestLooksList.classList.add('visible-scroll');
     app.scrollbarDiv.classList.remove('scrollbar-div-visible');
   },
-  // 4.3 removeScroll() does the opposite of addScroll()
+  // 5.3 removeScroll() does the opposite of addScroll()
   removeScroll: e => {
     app.latestLooksList.classList.remove('visible-scroll');
     app.scrollbarDiv.classList.add('scrollbar-div-visible');
